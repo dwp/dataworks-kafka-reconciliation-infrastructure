52800382c04f5625c602a20483a011657e75b6e0
Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>
2021-07-29 16:27:43 +0100

    Terraform house keeping
    Signed-off-by: Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>

--- lambda_glue_launcher.tf
+++ lambda_glue_launcher.tf
@@ -1,34 +1,3 @@
-variable "glue_launcher_zip" {
-  type = map(string)
-
-  default = {
-    base_path = ""
-    version   = ""
-  }
-}
-
-locals {
-  manifest_bucket_id     = data.terraform_remote_state.aws-internal-compute.outputs.manifest_bucket.id
-  manifest_import_type   = "streaming_main"
-  manifest_snapshot_type = "incremental"
-  manifest_data_name     = data.terraform_remote_state.dataworks-aws-ingest-consumers.outputs.manifest_etl.database_name
-
-  manifest_s3_input_parquet_location = data.terraform_remote_state.aws-internal-compute.outputs.manifest_s3_prefixes.parquet
-
-  manifest_counts_parquet_table_name        = data.terraform_remote_state.dataworks-aws-ingest-consumers.outputs.manifest_etl.table_name_counts_parquet
-  manifest_mismatched_timestamps_table_name = data.terraform_remote_state.dataworks-aws-ingest-consumers.outputs.manifest_etl.table_name_mismatched_timestamps_parquet
-  missing_imports_parquet_table_name        = data.terraform_remote_state.dataworks-aws-ingest-consumers.outputs.manifest_etl.table_name_missing_imports_parquet
-  missing_exports_parquet_table_name        = data.terraform_remote_state.dataworks-aws-ingest-consumers.outputs.manifest_etl.table_name_missing_exports_parquet
-
-  manifest_s3_input_parquet_location_base = "s3://${local.manifest_bucket_id}/${local.manifest_s3_input_parquet_location}/${local.manifest_import_type}_${local.manifest_snapshot_type}"
-  manifest_s3_output_location             = data.terraform_remote_state.aws-ingestion.outputs.manifest_comparison_parameters.query_output_s3_prefix
-
-  manifest_etl_combined_name = data.terraform_remote_state.dataworks-aws-ingest-consumers.outputs.manifest_etl.job_name_combined
-
-  batch_corporate_storage_coalescer_name = data.terraform_remote_state.dataworks-aws-ingest-consumers.outputs.batch_job_queues.batch_corporate_storage_coalescer.name
-  batch_corporate_storage_coalescer_long_running_name = data.terraform_remote_state.dataworks-aws-ingest-consumers.outputs.batch_job_queues.batch_corporate_storage_coalescer_long_running.name
-}
-
 resource "aws_lambda_function" "glue_launcher" {
   filename      = "${var.glue_launcher_zip["base_path"]}/glue-launcher-${var.glue_launcher_zip["version"]}.zip"
   function_name = "glue_launcher"
