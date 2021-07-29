896d811af917f8120fef5f4cbaba7a0d09044127
Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>
2021-07-29 11:32:54 +0100

    All env CI pipelines. Fix lambda env vars
    Signed-off-by: Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>

--- lambda_glue_launcher.tf
+++ lambda_glue_launcher.tf
@@ -45,24 +45,24 @@ resource "aws_lambda_function" "glue_launcher" {
       ENVIRONMENT                                              = local.environment
       APPLICATION                                              = "glue_launcher"
       LOG_LEVEL                                                = "INFO"
-      JOB_QUEUE_DEPENDENCIES                                   = "${data.terraform_remote_state.dataworks-aws-ingest-consumers.outputs.batch_job_queues.batch_corporate_storage_coalescer},${data.terraform_remote_state.dataworks-aws-ingest-consumers.outputs.batch_job_queues.batch_corporate_storage_coalescer_long_running}"
+      JOB_QUEUE_DEPENDENCIES                                   = "${data.terraform_remote_state.dataworks-aws-ingest-consumers.outputs.batch_job_queues.batch_corporate_storage_coalescer.name},${data.terraform_remote_state.dataworks-aws-ingest-consumers.outputs.batch_job_queues.batch_corporate_storage_coalescer_long_running.name}"
       ETL_GLUE_JOB_NAME                                        = data.terraform_remote_state.dataworks-aws-ingest-consumers.outputs.manifest_etl.job_name_combined
       MANIFEST_COUNTS_PARQUET_TABLE_NAME                       = "${local.manifest_data_name}.${local.manifest_counts_parquet_table_name}_${local.manifest_import_type}_${local.manifest_snapshot_type}"
       MANIFEST_MISMATCHED_TIMESTAMPS_TABLE_NAME                = "${local.manifest_data_name}.${local.manifest_mismatched_timestamps_table_name}_${local.manifest_import_type}_${local.manifest_snapshot_type}"
       MANIFEST_MISSING_IMPORTS_TABLE_NAME                      = "${local.manifest_data_name}.${local.missing_imports_parquet_table_name}_${local.manifest_import_type}_${local.manifest_snapshot_type}"
       MANIFEST_MISSING_EXPORTS_TABLE_NAME                      = "${local.manifest_data_name}.${local.missing_exports_parquet_table_name}_${local.manifest_import_type}_${local.manifest_snapshot_type}"
-      MANIFEST_S3_INPUT_LOCATION_IMPORT_HISTORIC               = data.terraform_remote_state.aws-internal-compute.outputs.manifest_comparison_parameters.historic_folder
-      MANIFEST_S3_INPUT_LOCATION_EXPORT_HISTORIC               = data.terraform_remote_state.aws-ingestion.outputs.manifest_comparison_parameters.query_output_s3_prefix
-      MANIFEST_COMPARISON_CUT_OFF_DATE_START                   = "PREVIOUS_DAY_MIDNIGHT" # Set an YYYY-MM-DDTHH:MM:SS.MMM to define. Else lambda will calculate date if empty or set to "PREVIOUS_DAY_MIDNIGHT"
-      MANIFEST_COMPARISON_CUT_OFF_DATE_END                     = "TODAY_MIDNIGHT" # Set an YYYY-MM-DDTHH:MM:SS.MMM to define. Else lambda will calculate date if empty or set to "TODAY_MIDNIGHT"
-      MANIFEST_COMPARISON_MARGIN_OF_ERROR_MINUTES              = "2" # Lambda defaults to 2 if not set
+      MANIFEST_S3_INPUT_LOCATION_IMPORT                        = data.terraform_remote_state.aws-internal-compute.outputs.manifest_comparison_parameters.historic_folder
+      MANIFEST_S3_INPUT_LOCATION_EXPORT                        = data.terraform_remote_state.aws-ingestion.outputs.manifest_comparison_parameters.query_output_s3_prefix
+      MANIFEST_COMPARISON_CUT_OFF_DATE_START                   = "PREVIOUS_DAY_MIDNIGHT" # Set an 'YYYY-MM-DD HH:MM:SS.MMM' to define. Else lambda will calculate date if empty or set to "PREVIOUS_DAY_MIDNIGHT"
+      MANIFEST_COMPARISON_CUT_OFF_DATE_END                     = "TODAY_MIDNIGHT"        # Set an 'YYYY-MM-DD HH:MM:SS.MMM' to define. Else lambda will calculate date if empty or set to "TODAY_MIDNIGHT"
+      MANIFEST_COMPARISON_MARGIN_OF_ERROR_MINUTES              = "2"                     # Lambda defaults to 2 if not set
       MANIFEST_COMPARISON_SNAPSHOT_TYPE                        = local.manifest_snapshot_type
       MANIFEST_COMPARISON_IMPORT_TYPE                          = local.manifest_import_type
       MANIFEST_S3_INPUT_PARQUET_LOCATION_MISSING_IMPORT        = "${local.manifest_s3_input_parquet_location_base}/missing_import"
       MANIFEST_S3_INPUT_PARQUET_LOCATION_MISSING_EXPORT        = "${local.manifest_s3_input_parquet_location_base}/missing_export"
       MANIFEST_S3_INPUT_PARQUET_LOCATION_COUNTS                = "${local.manifest_s3_input_parquet_location_base}/counts"
       MANIFEST_S3_INPUT_PARQUET_LOCATION_MISMATCHED_TIMESTAMPS = "${local.manifest_s3_input_parquet_location_base}/mismatched_timestamps"
-      MANIFEST_S3_OUTPUT_LOCATION                              = "s3://${local.manifest_bucket_id}/${manifest_s3_output_location}_${local.manifest_import_type}_${local.manifest_snapshot_type}/templates"
+      MANIFEST_S3_OUTPUT_LOCATION                              = "s3://${local.manifest_bucket_id}/${local.manifest_s3_output_location}_${local.manifest_import_type}_${local.manifest_snapshot_type}/templates"
     }
   }
 
