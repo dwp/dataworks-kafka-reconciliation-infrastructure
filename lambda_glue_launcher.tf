39142c8a1f38ab75fbf15255048de179caa9f3ea
Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>
2021-08-04 14:29:33 +0100

    Athena IAM
    Signed-off-by: Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>

--- lambda_glue_launcher.tf
+++ lambda_glue_launcher.tf
@@ -102,6 +102,7 @@ data "aws_iam_policy_document" "glue_launcher_lambda" {
     actions = [
       "athena:StartQueryExecution",
       "athena:GetQueryExecution",
+      "athena:GetQueryResults"
     ]
     resources = [
       "*"
