d76082e45b44a97a672e9191b365efb10c31bbd9
Mark <mark.stevens@infinityworks.com>
2021-08-04 11:45:41 +0100

    array json
    Signed-off-by: Mark <mark.stevens@infinityworks.com>

--- lambda_athena_reconciliation_launcher.tf
+++ lambda_athena_reconciliation_launcher.tf
@@ -26,15 +26,15 @@ resource "aws_lambda_function" "athena_reconciliation_launcher" {
       BATCH_JOB_NAME             = local.kafka_reconciliation_application_name
       BATCH_JOB_QUEUE            = aws_batch_job_queue.kafka_reconciliation.name
       BATCH_JOB_DEFINITION_NAME  = aws_batch_job_definition.kafka_reconciliation.name
-      BATCH_PARAMETERS_JSON = jsonencode({
-        "-i" : local.missing_imports_parquet_table_name,
-        "-e" : local.missing_exports_parquet_table_name,
-        "-c" : local.manifest_counts_parquet_table_name,
-        "-t" : local.manifest_mismatched_timestamps_table_name,
-        "-r" : local.manifest_report_count_of_ids,
-        "-p" : local.manifest_s3_output_location,
-        "-b" : local.manifest_bucket_id
-      })
+      BATCH_PARAMETERS_JSON = jsonencode([
+        "-i" , local.missing_imports_parquet_table_name,
+        "-e" , local.missing_exports_parquet_table_name,
+        "-c" , local.manifest_counts_parquet_table_name,
+        "-t" , local.manifest_mismatched_timestamps_table_name,
+        "-r" , local.manifest_report_count_of_ids,
+        "-p" , local.manifest_s3_output_location,
+        "-b" , local.manifest_bucket_id
+      ])
     }
   }
 
